bea00929cf46fc8263418a715816117b650ca656
DavidCa25 <banerdavid29@gmail.com>
2024-12-07 12:01:07 -0600

    [Fix] Arreglo diseño navbar

--- eventify/lib/presentation/widgets/Navbar.dart
+++ eventify/lib/presentation/widgets/Navbar.dart
@@ -4,43 +4,82 @@ import 'package:go_router/go_router.dart';
 
 class NavbarWidget extends StatelessWidget {
   final int index;
+
   const NavbarWidget({super.key, required this.index});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 375,
-      height: 75,
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
+    return Positioned(
+      bottom: 30,
+      left: 40,
+      right: 40,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        height: 75,
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[300],
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 10,
+              offset: const Offset(0, 5),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildNavItem(
+              context,
+              icon: Icons.home,
+              selected: index == 0,
+              onTap: () => context.go("/home/0"),
+            ),
+            _buildNavItem(
+              context,
+              icon: Icons.chat_bubble_outline,
+              selected: index == 1,
+              onTap: () => context.go("/home/1"),
+            ),
+          ],
+        ),
       ),
-      child: CurvedNavigationBar(
-        index: index,
-        backgroundColor: Colors.white, 
-        onTap: (value) {
-          switch (value) {
-            case 0:
-              context.go("/home/0"); 
-              break;
-            case 1:
-              context.go("/home/1"); 
-              break;
-          }
+    );
+  }
+
+  Widget _buildNavItem(
+    BuildContext context, {
+    required IconData icon,
+    required bool selected,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        transitionBuilder: (Widget child, Animation<double> animation) {
+          return ScaleTransition(
+            scale: animation,
+            child: child,
+          );
         },
-        color: Colors.amber[100]!, 
-        items: [
-          Icon(
-            Icons.home,
-            color: index == 0 ? Colors.black : Colors.blue, 
-            size: 32,
+        child: Container(
+          key: ValueKey<int>(selected ? 1 : 0), // Clave única para activar la animación
+          width: 45,
+          height: 45,
+          decoration: BoxDecoration(
+            color: selected ? Colors.white : Colors.transparent,
+            shape: BoxShape.circle,
           ),
-          Icon(
-            Icons.map_outlined,
-            color: index == 1 ? Colors.black : Colors.blue,
-            size: 32,
+          child: Icon(
+            icon,
+            color: selected ? Colors.black : Colors.white,
+            size: 24,
           ),
-        ],
+        ),
       ),
     );
   }
 }
+
