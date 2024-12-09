55fdc336d02cf7b00bce22408442ead781a2d488
Luna-hashbrown <danyjmoon@gmail.com>
2024-12-08 20:43:48 -0600

    Agregué api maps

--- eventify/lib/presentation/screens/home/map_view/map_view.dart
+++ eventify/lib/presentation/screens/home/map_view/map_view.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 
-class MapView extends StatelessWidget {
+
+class MapView extends StatefulWidget {
   const MapView({super.key});
 
   @override
+  State<MapView> createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return GoogleMap(
+      initialCameraPosition: 
+        const CameraPosition(target: LatLng(21.128010, -101.687023), 
+        zoom: 12),
+    );
   }
 }
